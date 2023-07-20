17b1b5ebb5e0f751b6c6b92d4cc55d74228d03b1
GUBONWOO <zcwxzsx@gmail.com>
2023-07-20 11:39:33 +0900

    Finish basic

---
+++ vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <h1>{{ prosdata }}</h1>
+    <button v-on:click="sendEvent">send</button>
+  </header>
+</template>
+
+<script>
+export default {
+  props: ['prosdata'],
+  methods: {
+    sendEvent: function () {
+      this.$emit('renew');
+    },
+  },
+};
+</script>
+
+<style></style>
